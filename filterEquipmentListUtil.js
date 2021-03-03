a551feffd11342b79cbb3842490e4582b2c08e0f
Larissa Brown <larisa.brown.johnson@gmail.com>
2021-03-03 14:17:13 -0700

    readme file note

--- filterEquipmentListUtil.js
+++ filterEquipmentListUtil.js
@@ -3,20 +3,14 @@ const equipmentList = require('./equipmentList');
 
 
 
+
 const  filteredData = (arr) => {
 
   const jsonOutput = JSON.stringify(arr, null, 2)
-  
   return fs.writeFileSync("./equipmentListJson.json",jsonOutput)
- 
-  
-  
-  
 };
 
-
-filteredData(equipmentList)
-
+  filteredData(equipmentList)
 
 
 
